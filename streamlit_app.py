be3f5d1e5517c44dd521c9ad0ca68ebd145dd345
Elias Ibrahim <elie.ibrahim@gmail.com>
2023-06-01 16:37:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,10 @@ else:
             # Iterate over the regex matches and prompt for field names
             for i, match in enumerate(regex_matches, start=1):
                 field_name = st.text_input(f"Enter field name for Token {i} (Value: {match.group()})", key=f"field_name_{i}", max_chars=20)
-                regex_pattern = f"(?P<{field_name}>{re.escape(match.group())})" if field_name else None
+                if not field_name:
+                    st.warning(f"Field name is required for Token {i}")
+                    continue
+                regex_pattern = f"(?P<{field_name}>{re.escape(match.group())})"
                 field_mapping_data.append({"Field Name": field_name, "Value": match.group(), "Regex Pattern": regex_pattern})
 
 # Create a DataFrame from the field mappings
@@ -114,6 +117,7 @@ if st.button("Generate SQL") and selected_fields:
     select_statement = "SELECT " + ", ".join(selected_fields) + f" FROM @{stage_name}/{selected_entry} (file_format => {selected_file_format})"
     st.write("Generated Select Statement:")
     st.code(select_statement)
+
 
     if st.button("Generate Regex") and selected_regex_patterns:
         combined_regex = "|".join(selected_regex_patterns)
